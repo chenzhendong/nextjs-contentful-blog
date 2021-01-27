0eba207a1b8771ae386a4f0b0efbce5abc70a070
Zhendong Chen <zchen@Zhendongs-MBP.fios-router.home>
2021-01-27 00:30:13 -0500

    implement api call

--- lib/data-contentful.tsx
+++ lib/data-contentful.tsx
@@ -1,30 +1,58 @@
 import { createClient } from "contentful";
-import { symlinkSync } from "fs";
 
-export function getBogPosts() {
+const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID as string,
+    accessToken: process.env.CONTENTFUL_TOKEN as string
+});
 
+export async function getBlogPosts() {
+    const bps = await client.getEntries({
+        content_type: "blogPost",
+        order: '-fields.publishDate'
+    });
+    if(bps.items) {
+        return bps.items;
+    }
+    return undefined;
+}
+
+export async function getBlogPostsByCategory(categoryName: string) {
+    const bps = await client.getEntries({
+        content_type: "blogPost",
+        'fields.tags': categoryName,
+        order: '-fields.publishDate'
+    });
+    if(bps.items) {
+        return bps.items;
+    }
+    return undefined;
+}
+
+export async function getCategories() {
+    const t = await client.getContentType('blogPost')
+    for ( const f of t.fields) {
+        if ( f.name === "Tags") {
+            return f.items?.validations[0].in as Array<string>;
+        }
+    }
+    return [] as Array<string>;
+}
+
+export async function getPerson() {
+    const p = await client.getEntries({content_type: "person"});
+    if(p.items) {
+        return p.items[0];
+    }
+    return undefined;
 }
 
 
 (async () => {
     try{
-        const client = createClient({
-            space: process.env.CONTENTFUL_SPACE_ID as string,
-            accessToken: process.env.CONTENTFUL_TOKEN as string
-        });
-        const entries = await client.getEntries();
-        entries.items.forEach( (entry:any) => {
-            const fields = entry.fields;
-            console.log(entry.sys.space)
-            console.log(entry.sys.contentType)
-            if(fields) {
-                const image = fields.image;
-                if(image) {
-                    console.log(image)
-                }                
-            }
-            
-        });
+        const bps = await getBlogPostsByCategory("General");
+        if(bps) {
+            console.log(bps[0])
+        }
     } catch (e) {
         console.log(e)
     }
